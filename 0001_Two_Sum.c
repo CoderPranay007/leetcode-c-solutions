7d3197ff422ef410447b3ac934edfee36b22ee2a
CoderPranay007 <pranaykhera.pk@gmail.com>
2026-06-09 09:19:52 +0530

    Add solution for LeetCode 1: Two Sum
    Two Sum problem solution using a nested loop.

---
+++ 0001_Two_Sum.c
@@ -0,0 +1,31 @@
+/*
+ * LeetCode 1. Two Sum
+ * Difficulty: Easy
+ * Topics: Array, Hash Table
+ *
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ */
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int *result = (int *)malloc(2 * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    free(result);
+    return NULL;
+}
